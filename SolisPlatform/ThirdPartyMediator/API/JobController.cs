80b72fa2f420e677b0e26730fe210d27d3882bec
Haider Zaidi <haiderzaidi94@users.noreply.github.com>
2019-11-08 01:22:25 +0500

    Initial Commit
    Hangfire and SelfHost setup

---
+++ SolisPlatform/ThirdPartyMediator/API/JobController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThirdPartyMediator.API
+{
+    [Route("api/job")]
+    public class JobController
+    {
+        Thread t;
+        public JobController() {
+            
+        }
+
+        [Route("initiate")]
+        [HttpGet]
+        public void Index() {
+            
+        }
+
+        
+    }
+}
---
+++ SolisPlatform/ThirdPartyMediator/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace ThirdPartyMediator
+{
+    public class Startup
+    {
+        IConfigurationRoot Configuration;
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+
+            Configuration = builder.Build();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(configuration => configuration
+            .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseSqlServerStorage(Configuration["ConnectionStrings:HangfireConnection"], new SqlServerStorageOptions
+            {
+                //CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                //SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                //QueuePollInterval = TimeSpan.Zero,
+                UseRecommendedIsolationLevel = true,
+                UsePageLocksOnDequeue = true,
+                DisableGlobalLocks = true
+            }));
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseHangfireDashboard();
+            app.UseMvc();
+        }
+    }
+}
